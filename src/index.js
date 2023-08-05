7a5980b203af66f01371dabd985996e7d15f5f65
Tano Paul <tanopaul38@gmail.com>
2023-08-05 12:09:04 -0400

    completed fetch on demand forms phase 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+const ul = document.getElementById('book-list')
+
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let id = e.target.children[1].value;
+
+        fetch(`http://localhost:3000/movies/${id}`)
+        .then(resp => resp.json())
+        .then(movie => {
+            let summary = document.getElementById('summary');
+            let title = document.getElementById('title');
+            
+            summary.innerText = movie.summary;
+            title.innerText = movie.title;
+        })
+        
+      
+    })
+    
 }
 
 document.addEventListener('DOMContentLoaded', init);
